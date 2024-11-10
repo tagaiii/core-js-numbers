54cdc18452444ae4fbcf300299e7afda4dda820a
Kamalov Tagaibek <tagai.nambavan@gmail.com>
2024-11-10 16:00:23 +0600

    feat: solve 21 of 56 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -273,8 +273,17 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index <= 1) {
+    return index;
+  }
+  let prev = 0;
+  let curr = 1;
+  for (let i = 2; i <= index; i += 1) {
+    const next = prev + curr;
+    [prev, curr] = [curr, next];
+  }
+  return curr;
 }
 
 /**
@@ -288,8 +297,12 @@ function getFibonacciNumber(/* index */) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let sum = n;
+  for (let i = 1; i < n; i += 1) {
+    sum += i;
+  }
+  return sum;
 }
 
 /**
@@ -303,8 +316,18 @@ function getSumToN(/* n */) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  let sum = 0;
+  let currentNum = num;
+  if (num < 10) {
+    return num;
+  }
+  while (currentNum > 0) {
+    const lastDigit = currentNum % 10;
+    sum += lastDigit;
+    currentNum = (currentNum - lastDigit) / 10;
+  }
+  return sum;
 }
 
 /**
@@ -318,8 +341,8 @@ function getSumOfDigits(/* num */) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  return 2 ** Math.round(Math.log2(num)) === num;
 }
 
 /**
@@ -332,8 +355,8 @@ function isPowerOfTwo(/* num */) {
  *   0 => 0
  *   Math.PI / 2 => 1
  */
-function getSine(/* num */) {
-  throw new Error('Not implemented');
+function getSine(num) {
+  return Math.sin(num);
 }
 
 /**
